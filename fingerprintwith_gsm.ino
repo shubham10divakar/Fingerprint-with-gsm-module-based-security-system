7e6a99fc4bdb72405837cc7b993ccd4a23b06580
shubham10divakar <31129406+shubham10divakar@users.noreply.github.com>
2018-03-30 20:24:18 +0530

    Add files via upload

---
+++ fingerprintwith_gsm.ino
@@ -0,0 +1,203 @@
+/*************************************************** 
+  This is an example sketch for our optical Fingerprint sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> http://www.adafruit.com/products/751
+
+  These displays use TTL Serial to communicate, 2 pins are required to 
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+
+#include <Adafruit_Fingerprint.h>
+#include <Servo.h>
+Servo servo_test;
+int angle=0;
+
+// On Leonardo/Micro or others with hardware serial, use those! #0 is green wire, #1 is white
+// uncomment this line:
+// #define mySerial Serial1
+
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// comment these two lines if using hardware serial
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+void SendSMS(void);
+void SendSMS2(void);
+void Running(void);
+void setup()  
+{
+   servo_test.attach(9);// put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial);  // For Yun/Leo/Micro/Zero/...
+  delay(100);
+  Serial.println("\n\nAdafruit finger detect test");
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+  finger.getTemplateCount();
+  Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+  Serial.println("Waiting for valid finger...");
+}
+
+void loop()                     // run over and over again
+{
+  getFingerprintIDez();
+  delay(50);            //don't ned to run this at full speed.
+}
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("No finger detected");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+
+  // OK success!
+
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return p;
+    default:
+      Serial.println("Unknown error");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Found a print match!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("Did not find a match");
+    return p;
+  } else {
+    Serial.println("Unknown error");
+    return p;
+  }   
+  
+  // found a match!
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence); 
+
+  return finger.fingerID;
+}
+
+// returns -1 if failed, otherwise returns ID #
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK){ // Serial.println("NOT MATCHED");
+ // SendSMS();
+    return -1;
+    
+  }
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  { //Serial.println("NOT MATCHED");
+    //SendSMS();
+    return -1;
+     
+  }
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  {
+    Serial.println("NOT MATCHED");
+    SendSMS();
+    return -1;
+     
+  }
+  
+  // found a match!
+ // SendSMS2();
+  Serial.print("Found ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  SendSMS2();
+  Running();
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  delay(20000);
+//  digitalWrite(8,LOW);
+//  digitalWrite(7,LOW);
+  return finger.fingerID; 
+}
+void SendSMS()
+{
+  Serial.println("AT+CMGF=1");    //To send SMS in Text Mode
+  delay(1000);
+  Serial.println("AT+CMGS=\"+919110968219\"\r"); //Change to destination phone number 
+  delay(1000);
+  Serial.println("Hello from GSM Modem!!UNIDENTIFIED PERSON AT THE DOOR");//the content of the message
+  delay(200);
+  Serial.println((char)26); //the stopping character Ctrl+Z
+  delay(1000);  
+}
+void SendSMS2()
+{
+  Serial.println("AT+CMGF=1");    //To send SMS in Text Mode
+  delay(1000);
+  Serial.println("AT+CMGS=\"+919110968219\"\r"); //Change to destination phone number 
+  delay(1000);
+  Serial.println("Hello from GSM Modem!!LOCK IS OPENED");//the content of the message
+  delay(200);
+  Serial.println((char)26); //the stopping character Ctrl+Z
+  delay(1000);  
+}
+void Running()
+{
+    delay(100);
+  for(angle=180;angle>=0;angle-=60)
+{
+  servo_test.write(angle);
+  delay(5);
+}
+delay(1000);
+}//running
+
